335690e9d1a5a52ab1dec739ae5d6e75ba4e9e3b
Carlos <cmsandoval2@miners.utep.edu>
2023-02-27 11:57:22 -0700

    added way to end the game at start

--- omok/src/omok/ConsoleUI.java
+++ omok/src/omok/ConsoleUI.java
@@ -7,7 +7,7 @@ public class ConsoleUI {
 	GameController g=new GameController();
 public void intro() {
 	System.out.println("Welcome");
-	System.out.println("Choose game mode 1 for computer 2 for PVP");
+	System.out.println("Choose game mode 1 for computer 2 for PVP, or 3 to quit game");
 	int num=scnr.nextInt();
 	switch(num) {
 	case 1:
@@ -15,6 +15,10 @@ public void intro() {
 		break;
 	case 2:
 		g.PVP();
+	case 3:
+		break;	
+	default:
+			break;
 	}
 	//System.out.println("Choose Game mode 1 for computer game or 2 for PVP");
 	//System.out.println("Choose Row");
