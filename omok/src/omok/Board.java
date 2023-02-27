f1c7a5a6afa35687f60ec71f6489432a5a26014e
Carlos <cmsandoval2@miners.utep.edu>
2023-02-26 20:36:22 -0700

    added the winner of the game and a pvp method

--- omok/src/omok/Board.java
+++ omok/src/omok/Board.java
@@ -1,24 +1,6 @@
 package omok;
 
 public class Board {
-/*public char[][] board= {{'X',' ','1',' ','2',' ','3',' ','4',' ','5',' ','6',' ','7',' ','8',' ','9',' ','a',' ','b',' ','c',' ','d',' ','e',' ','f'},
-						{'y','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
-						{'1','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'2','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'3','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'4','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'5','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'6','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'7','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'8','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'9','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'a','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'b','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'c','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'d','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'e','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'},
-						{'f','|','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.',' ','.'}};
-						*/
 	public char[][] board= {
 			{'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
 			{'.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},
@@ -54,10 +36,92 @@ public void displayboard() {
 
 public boolean isFull() {
 	// TODO Auto-generated method stub
+	
 	return false;
 }
-public boolean isWinner() {
+public boolean isWinner(String player) {
 	// TODO Auto-generated method stub
+	if(player=="player") {
+		//Horizontal
+	for(int i=0;i<board.length;i++) {
+		for(int j=0;j<board[i].length-3;j++) {
+			if(board[i][j]=='X'&&board[i][j+1]=='X'&&board[i][j+2]=='X'&&board[i][j+3]=='X'&&board[i][j+4]=='X') {
+				return true;
+			}
+		}
+	}
+			//vertical
+		for(int i=0;i<board.length-3;i++) {
+			for(int j=0;j<board[i].length;j++) {
+		 if(board[i][j]=='X'&&board[i+1][j]=='X'&&board[i+2][j]=='X'&&board[i+3][j]=='X'&&board[i+4][j]=='X') {
+				return true;
+			}
+			}
+		}
+		for(int i=board.length-3; i>=0; i--) {
+		    for(int j=0; j<board[i].length-3; j++) {
+		        if(board[i][j]=='X'&&board[i+1][j+1]=='X'&&board[i+2][j+2]=='X'&&board[i+3][j+3]=='X'&&board[i+4][j+4]=='X') {
+		            return true;
+		        }
+		    }
+		}
+		//Ascending diagonal
+		//for(int i=4;i<board.length;i++) {
+			//for(int j=4;j<board[i].length;j++) {
+				//if(board[i][j]=='X'&&board[i+1][j-1]=='X'&&board[i+2][j-2]=='X'&&board[i+3][j-3] =='X'&&board[i+4][j-4]=='X') {
+					//return true;
+		//}
+			//}
+		//}
+		//Descending diagonal
+		for(int i=4;i<board.length;i++) {
+			for(int j=4;j<board[i].length;j++) {
+				if(board[i][j]=='X'&&board[i-1][j-1]=='X'&&board[i-2][j-2]=='X'&&board[i-3][j-3] =='X'&&board[i-4][j-4]=='X') {
+					return true;
+		}
+			}
+		}
+	}
+	if(player=="cpu"||player=="player2") {
+		//Horizontal
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board[i].length-3;j++) {
+				if(board[i][j]=='O'&&board[i][j+1]=='O'&&board[i][j+2]=='O'&&board[i][j+3]=='O'&&board[i][j+4]=='O') {
+					return true;
+				}
+			}
+		}
+				//vertical
+			for(int i=0;i<board.length-3;i++) {
+				for(int j=0;j<board[i].length;j++) {
+			 if(board[i][j]=='O'&&board[i+1][j]=='O'&&board[i+2][j]=='O'&&board[i+3][j]=='O'&&board[i+4][j]=='O') {
+					return true;
+				}
+				}
+			}
+			//Ascending diagonal
+			for(int i=board.length-5; i>=0; i--) {
+			    for(int j=0; j<board[i].length-4; j++) {
+			        if(board[i][j]=='X'&&board[i+1][j+1]=='X'&&board[i+2][j+2]=='X'&&board[i+3][j+3]=='X'&&board[i+4][j+4]=='X') {
+			            return true;
+			        }
+			    }
+			}
+			//for(int i=3;i<board.length;i++) {
+				//for(int j=0;j<board[i].length-3;j++) {
+			 //if (board[i][j]=='O'&&board[i-1][j+1]=='O'&&board[i-2][j+2]=='O'&&board[i-3][j+3]=='O'&&board[i-4][j-4]=='O')
+	           //     return true;
+				//}
+			//}
+			//Descending diagonal
+			for(int i=4;i<board.length;i++) {
+				for(int j=4;j<board[i].length;j++) {
+					if(board[i][j]=='O'&&board[i-1][j-1]=='O'&&board[i-2][j-2]=='O'&&board[i-3][j-3] =='O'&&board[i-4][j-4]=='O') {
+						return true;
+			}
+				}
+			}
+	}
 	return false;
 }
 public void placeStone(int row,int col,String user) {
@@ -65,7 +129,7 @@ public void placeStone(int row,int col,String user) {
 	char symbol=' ';
 	if(user.equals("player")) {
 		symbol='X';
-	}else if(user.equals("cpu")){
+	}else if(user.equals("cpu")||user.equals("player2")){
 		symbol='O';
 	}
 	board[row-1][col-1]=symbol;	
--- omok/src/omok/GameController.java
+++ omok/src/omok/GameController.java
@@ -8,23 +8,53 @@ public class GameController {
 	Board board=new Board();
 	public void computerGame() {
 		// TODO Auto-generated method stub
-		while(!board.isFull()||!board.isWinner()) {
+		//while(!board.isFull()||!board.isWinner()) {
+		while(true) {
 		board.displayboard();
 		System.out.println("Choose Row from 1-15");
 		int row=scnr.nextInt();
 		System.out.println("Choose Column from 1-15");
 		int col=scnr.nextInt();
 		board.placeStone(row,col,"player");
+		if(board.isWinner("player")) {
+			System.out.println("You won, Congrats!!!");
+			board.displayboard();
+			return;
+		}
 		Random rand=new Random();
 		int cpuRow=rand.nextInt(15)+1;
 		int cpuCol=rand.nextInt(15)+1;
 		board.placeStone(cpuRow, cpuCol, "cpu");
+		if(board.isWinner("cpu")) {
+			System.out.println("CPU has won");
+			board.displayboard();
+			return;
+		}
 		//board.displayboard();
 		}
 	}
 
 	public void PVP() {
 		// TODO Auto-generated method stub
+		while(true) {
+			board.displayboard();
+			System.out.println("Choose Row from 1-15");
+			int row=scnr.nextInt();
+			System.out.println("Choose Column from 1-15");
+			int col=scnr.nextInt();
+			board.placeStone(row,col,"player");
+			if(board.isWinner("player")) {
+				System.out.println("You won, Congrats!!!");
+				board.displayboard();
+				return;
+			}
+			System.out.println("Player 2 turn");
+			System.out.println("Choose Row from 1-15");
+			int p2row=scnr.nextInt();
+			System.out.println("Choose Column from 1-15");
+			int p2col=scnr.nextInt();
+			board.placeStone(p2row,p2col,"player2");
+	}
 		
 	}
 
